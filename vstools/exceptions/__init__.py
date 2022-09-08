2ca60b18ecd5f2943a23a40ead08ec790b99a3fb
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-09-08 23:51:55 +0200

    Add OS related exceptions

--- vstools/exceptions/__init__.py
+++ vstools/exceptions/__init__.py
@@ -1,4 +1,5 @@
+from .base import *  # noqa: F401, F403
 from .color import *  # noqa: F401, F403
 from .enum import *  # noqa: F401, F403
+from .file import *  # noqa: F401, F403
 from .generic import *  # noqa: F401, F403
-from .base import *  # noqa: F401, F403
