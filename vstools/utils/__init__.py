c755524df6cbbcbe0f387debc49359b789b235ef
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-09-09 20:22:20 +0200

    Remove mime_base from __init__

--- vstools/utils/__init__.py
+++ vstools/utils/__init__.py
@@ -2,7 +2,6 @@ from .info import *  # noqa: F401, F403
 from .math import *  # noqa: F401, F403
 from .ffprobe import *  # noqa: F401, F403
 from .file import *  # noqa: F401, F403
-from .mime_base import *  # noqa: F401, F403
 from .mime import *  # noqa: F401, F403
 from .misc import *  # noqa: F401, F403
 from .props import *  # noqa: F401, F403
