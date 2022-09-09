9a9183bb0939d04d5caf7d9ca0953b8f6a22252a
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-09-09 22:30:36 +0200

    Add metadata

---
+++ vstools/_metadata.py
@@ -0,0 +1,12 @@
+"""Functions and utils related to VapourSynth"""
+
+__version__ = '1.0.0'
+
+__author__ = 'Setsugen no ao <setsugen@setsugen.dev>'
+__maintainer__ = __author__
+
+__author_name__, __author_email__ = [x[:-1] for x in __author__.split('<')]
+__maintainer_name__, __maintainer_email__ = [x[:-1] for x in __maintainer__.split('<')]
+
+if __name__ == '__github__':
+    print(__version__)
