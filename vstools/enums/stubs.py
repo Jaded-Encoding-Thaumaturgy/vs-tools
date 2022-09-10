9a6c9697b4506c53c50133bdfd44c28967d82556
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-09-10 19:56:45 +0200

    Remove unused type ignore

--- vstools/enums/stubs.py
+++ vstools/enums/stubs.py
@@ -183,7 +183,7 @@ if TYPE_CHECKING:
         ) -> FieldBased | None:
             ...
 
-        @classmethod  # type: ignore
+        @classmethod
         def from_param(cls: Any, value: Any, func_except: Any = None) -> FieldBased | None:
             ...
 else:
