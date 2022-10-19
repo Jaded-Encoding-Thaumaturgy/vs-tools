15e219ab4310df1fbcddd4e0821b4d39f3572c30
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-10-19 13:13:10 +0200

    Add utils.other submodule

--- vstools/utils/__init__.py
+++ vstools/utils/__init__.py
@@ -7,6 +7,7 @@ from .info import *  # noqa: F401, F403
 from .math import *  # noqa: F401, F403
 from .mime import *  # noqa: F401, F403
 from .misc import *  # noqa: F401, F403
+from .other import *  # noqa: F401, F403
 from .props import *  # noqa: F401, F403
 from .ranges import *  # noqa: F401, F403
 from .scale import *  # noqa: F401, F403
