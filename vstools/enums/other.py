78836b8247c70541ceea04992a21a3e234321b8d
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-11-12 14:07:17 +0100

    Update `Dar`

--- vstools/enums/other.py
+++ vstools/enums/other.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from typing import NamedTuple
 
+import vapoursynth as vs
+
 from .base import CustomIntEnum, CustomStrEnum
 
 __all__ = [
@@ -22,9 +24,30 @@ class Direction(CustomIntEnum):
 class Dar(CustomStrEnum):
     """StrEnum signifying an analog television aspect ratio."""
 
-    WIDE = WIDESCREEN = 'widescreen'
-    FULL = FULLSCREEN = 'fullscreen'
+    WIDE = 'wide'
+    FULL = 'full'
     SQUARE = 'square'
+
+    @classmethod
+    def from_video(cls, src: vs.VideoNode | vs.VideoFrame | vs.FrameProps, strict: bool = False) -> Dar:
+        from ..exceptions import CustomValueError, FramePropError
+        from ..utils import get_prop
+
+        try:
+            sar = get_prop(src, "_SARDen", int), get_prop(src, "_SARNum", int)
+        except FramePropError:
+            if strict:
+                raise FramePropError(
+                    '', '', 'SAR props not found! Make sure your video indexing plugin sets them!'
+                )
+
+            return Dar.WIDESCREEN
+
+        match sar:
+            case (11, 10) | (9, 8): return Dar.FULLSCREEN
+            case (33, 40) | (27, 32): return Dar.WIDESCREEN
+
+        raise CustomValueError("Could not calculate DAR. Please set the DAR manually.")
 
 
 class Region(CustomStrEnum):
