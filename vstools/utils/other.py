39b4128cb7449a184f44a5def7109258d8fb2498
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-12-13 02:02:55 +0100

    Add `get_nvidia_version`

--- vstools/utils/other.py
+++ vstools/utils/other.py
@@ -1,10 +1,12 @@
 import builtins
 import os
 import sys
+from subprocess import run
 from typing import TYPE_CHECKING
 
 __all__ = [
-    'IS_DOCS'
+    'IS_DOCS',
+    'get_nvidia_version'
 ]
 
 IS_DOCS = not TYPE_CHECKING and (
@@ -15,3 +17,22 @@ IS_DOCS = not TYPE_CHECKING and (
     # in conf.py you can do builtins.__sphinx_build__ = True to be 100% sure
     or (hasattr(builtins, '__sphinx_build__') and builtins.__sphinx_build__)
 )
+
+
+def get_nvidia_version() -> tuple[int, int] | None:
+    nvcc = run(['nvcc', '--version'], capture_output=True)
+
+    ver_string = ''
+
+    if nvcc.returncode:
+        smi = run(['nvidia-smi', '-q'], capture_output=True)
+
+        if not smi.returncode:
+            ver_string = smi.stdout.splitlines()[5].decode().split(':')[-1].strip()
+    else:
+        ver_string = nvcc.stdout.splitlines()[3].decode().split(',')[-2].replace('release', '').strip()
+
+    if not ver_string:
+        return None
+
+    return tuple(int(x) for x in ver_string.split('.', 2))  # type: ignore
