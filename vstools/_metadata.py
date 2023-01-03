814ba73608eddc61a3c041bb4cb95bf9fbd23aa3
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2023-01-03 21:54:19 +0100

    Bump minor version

--- vstools/_metadata.py
+++ vstools/_metadata.py
@@ -1,6 +1,6 @@
 """Functions and utils related to VapourSynth"""
 
-__version__ = '1.6.10'
+__version__ = '1.6.11'
 
 __author__ = 'Setsugen no ao <setsugen@setsugen.dev>'
 __maintainer__ = __author__
