2bc945018225c456aa428284f0f6b3a16d1b4341
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2023-01-30 18:12:31 +0100

    Use dict keys properties to make an ordered set

--- vstools/exceptions/generic.py
+++ vstools/exceptions/generic.py
@@ -167,8 +167,8 @@ class MismatchError(CustomValueError):
         return str(item)
 
     @classmethod
-    def _reduce(cls, items: Iterable[T]) -> set[str]:
-        return set(map(cls._item_to_name, items))
+    def _reduce(cls, items: Iterable[T]) -> tuple[str]:
+        return tuple(dict.fromkeys(map(cls._item_to_name, items)).keys())
 
     def __init__(
         self, func: FuncExceptT, items: Iterable[T], message: SupportsString = 'All items must be equal!',
