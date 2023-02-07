a3de7bd1ad826b17997ca44d20393720d0070cd8
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2023-02-07 14:44:13 +0100

    Add SentinelDispatcher

--- vstools/types/funcs.py
+++ vstools/types/funcs.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
+from functools import wraps
 
-from typing import TYPE_CHECKING, Any, Iterable, List, SupportsIndex, overload
+from typing import TYPE_CHECKING, Any, Callable, Iterable, List, SupportsIndex, TypeVar, overload
 
 import vapoursynth as vs
 
-from .builtins import SupportsString
+from .builtins import T, SupportsString, P
 
 __all__ = [
-    'StrList'
+    'StrList',
+    'Sentinel'
 ]
 
 
@@ -61,3 +63,48 @@ class StrList(List[SupportsString]):
     def append(self, *__object: SupportsString) -> None:
         for __obj in __object:
             super().append(__obj)
+
+
+class SentinelDispatcher:
+    def check(self, ret_value: T, cond: bool) -> T | SentinelDispatcher:
+        return ret_value if cond else self
+
+    def check_cb(self, callback: Callable[P, tuple[T, bool]]) -> Callable[P, T | SentinelDispatcher]:
+        @wraps(callback)
+        def _wrap(*args: P.args, **kwargs: P.kwargs) -> T | SentinelDispatcher:
+            return self.check(*callback(*args, **kwargs))
+
+        return _wrap
+
+    def filter(self: SelfSentinel, items: Iterable[T | SelfSentinel]) -> Iterable[T]:
+        for item in items:
+            if item is self:
+                continue
+
+            yield item  # type: ignore
+
+    @classmethod
+    def filter_multi(cls, items: Iterable[T | SelfSentinel], *sentinels: SelfSentinel) -> Iterable[T]:
+        for item in items:
+            if item in sentinels:
+                continue
+
+            yield item  # type: ignore
+
+    def __getattr__(self, name: str) -> SentinelDispatcher:
+        if name not in _sentinels:
+            _sentinels[name] = SentinelDispatcher()
+        return _sentinels[name]
+
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        raise NameError
+
+    def __call__(self) -> SentinelDispatcher:
+        return SentinelDispatcher()
+
+
+Sentinel = SentinelDispatcher()
+
+_sentinels = dict[str, SentinelDispatcher]()
+
+SelfSentinel = TypeVar('SelfSentinel', bound=SentinelDispatcher)
