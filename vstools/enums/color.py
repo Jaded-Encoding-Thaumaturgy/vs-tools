d7615f4534664fc7490517e94cb090cc96b1f6bb
emotion3459 <176516814+emotion3459@users.noreply.github.com>
2024-08-15 08:47:57 -0400

    Add additional BT709 aliases

--- vstools/enums/color.py
+++ vstools/enums/color.py
@@ -77,6 +77,8 @@ class Matrix(_MatrixMeta):
     IEC 61966-2-4 xvYCC709
     SMPTE RP 177 (1993) Annex B
     """
+    BT1886 = BT709
+    GAMMA24 = BT709 # Not exactly, but since zimg assumes infinite contrast BT1886 is effectively GAMMA24 here.
 
     UNKNOWN = 2
     """Image characteristics are unknown or are determined by the application."""
