ee729df9dbfa981f2a164d4bb38aa11f449a2703
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2024-10-03 23:56:12 +0200

    OutdatedPluginError: New exception

--- vstools/exceptions/module.py
+++ vstools/exceptions/module.py
@@ -1,8 +1,23 @@
 from __future__ import annotations
 
-from stgpytools import CustomImportError, DependencyNotFoundError
+from typing import Any
+
+from stgpytools import CustomImportError, DependencyNotFoundError, FuncExceptT, SupportsString
 
 __all__ = [
     'CustomImportError',
-    'DependencyNotFoundError'
+    'DependencyNotFoundError',
+
+    'OutdatedPluginError'
 ]
+
+
+class OutdatedPluginError(CustomImportError):
+    """Raised when a plugin is outdated and needs to be updated."""
+
+    def __init__(
+        self, func: FuncExceptT, package: str | ImportError,
+        message: SupportsString = 'Plugin \'{package}\' version is too old. Please update to a more recent version.',
+        **kwargs: Any
+    ) -> None:
+        super().__init__(func, package, message, **kwargs)
