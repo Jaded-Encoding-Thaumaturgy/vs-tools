881b66a2f3e8843b838bdd36f28bf9b49a38f223
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-02-11 00:58:24 +0100

    bump minor version

--- vstools/_metadata.py
+++ vstools/_metadata.py
@@ -1,6 +1,6 @@
 """Functions and utils related to VapourSynth."""
 
-__version__ = '3.4.2'
+__version__ = '3.4.3'
 
 __author_name__, __author_email__ = 'Setsugen no ao', 'setsugen@setsugen.dev'
 __maintainer_name__, __maintainer_email__ = __author_name__, __author_email__
